358ca459f1f47db45187b05e831cb87b3b92043a
Kirill <yakovlev2@gmail.com>
2022-07-17 20:33:01 +0300

    Initial Commit

---
+++ ClimaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClimaApp
+//
+//  Created by Proshka Kirill on 17.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
